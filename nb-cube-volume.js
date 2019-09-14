3fedc706ed3a4ac0909ad2eeaecf9dcb4c3ade3b
Brad Evans <kaloba4r@gmail.com>
2019-09-14 16:14:44 -0400

    cube volume challenge

--- nb-cube-volume.js
+++ nb-cube-volume.js
@@ -1,30 +1,37 @@
-let volume = 0;
-let n = 1;
-
+let n = 0;
+let result = 0;
 const findNb = num => {
-  if (num == 1) {
-    volume += 1;
-    console.log(volume);
-    return volume;
+
+  if (num < 0) {
+    console.log('-1 is the return');
+    return result = -1;
+  } else if (num == 0) {
+    console.log(n - 1, 'is the return');
+    return result = (n - 1);
   } else {
-    num -= (n * n);
+    console.log(n);
+    num -= (n * n * n);
+    console.log(num)
     n += 1;
     findNb(num);
   }
-}
+  // console.log(result)
+  return result
+};
+
+//findNb(1071225) // Returns --> 45
+findNb(135440716410000)
 
 
-findNb(1);
-findNb(1071225)
-
-
-
-
-
-// findNb(1071225) // Returns --> 45
-// findNb(91716553919377) // Returns --> -1
-
+/// Optimization /// info for testing & optimization
+//findNb(1071225) // Returns --> 45
 // Test.assertEquals(findNb(4183059834009), 2022)
 // Test.assertEquals(findNb(24723578342962), -1)
 // Test.assertEquals(findNb(135440716410000), 4824)
 // Test.assertEquals(findNb(40539911473216), 3568)
+
+
+
+
+
+
