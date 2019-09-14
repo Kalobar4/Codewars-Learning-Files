34db681e4b259f31b8e9e6518c7787983dccadbb
Brad Evans <kaloba4r@gmail.com>
2019-09-14 04:53:44 -0400

    codewar level up to 7

---
+++ orderPlease.js
@@ -0,0 +1,13 @@
+const order = (str) => {
+  let result = [];
+  let arr = str.split(' ')
+  console.log(arr);
+  for (i = 0; i < arr.length; i++) { var result1 = arr[i].filter(() => { }}
+
+
+  // console.log(result)
+
+};
+
+
+order("is2 Thi1s T4est 3a")
