48229faddee691ccb2ffc4727b347880c2be2feb
Brad Evans <kaloba4r@gmail.com>
2019-09-27 22:33:53 -0400

    clean up and refactor

--- permutation.js
+++ permutation.js
@@ -1,6 +1,5 @@
 //  Challenge: calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle). The answer should be a number rounded to 3 decimal places.
 
-
 const areaOfPolygonInsideCircle = (circleRadius, numberOfSides) => {
   var area = (numberOfSides * circleRadius * circleRadius / 2 * Math.sin(2 * Math.PI / numberOfSides)).toFixed(3)
 
@@ -12,28 +11,3 @@ areaOfPolygonInsideCircle(3, 3) // returns 11.691
 areaOfPolygonInsideCircle(5.8, 7) // returns 92.053
 
 areaOfPolygonInsideCircle(4, 5) // returns 38.042
-
-
-
-
-
-
-
-
-const permutator = (inputArr) => {
-  let result = [];
-  const permute = (arr, m = []) => {
-    if (arr.length === 0) {
-      result.push(m)
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), m.concat(next))
-      }
-    }
-  }
-  permute(inputArr)
-
-  return result;
-}
