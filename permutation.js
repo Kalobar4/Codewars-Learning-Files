75bb9beadda2c1ed8865026bd3ee7aa3c9735239
Brad Evans <kaloba4r@gmail.com>
2019-09-27 22:32:01 -0400

    challenge detail added

--- permutation.js
+++ permutation.js
@@ -1,3 +1,6 @@
+//  Challenge: calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle). The answer should be a number rounded to 3 decimal places.
+
+
 const areaOfPolygonInsideCircle = (circleRadius, numberOfSides) => {
   var area = (numberOfSides * circleRadius * circleRadius / 2 * Math.sin(2 * Math.PI / numberOfSides)).toFixed(3)
 
