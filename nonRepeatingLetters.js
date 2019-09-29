34fb47fb24fb1da07b191f7bd7004ba1c2d6f1f6
Brad Evans <kaloba4r@gmail.com>
2019-09-29 19:32:23 -0400

    object solution to letter evaluation challenge

--- nonRepeatingLetters.js
+++ nonRepeatingLetters.js
@@ -1,7 +1,6 @@
 // Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.  For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.  As an added challenge, upper - and lowercase letters are considered the same character, but the function should return the correct case for the initial letter.For example, the input 'sTreSS' should return 'T'.  If a string contains all repeating characters, it should return an empty string("") or None-- see sample tests.
 
 const firstNonRepeatingLetter = str => {
-  console.log(str)
   const arr = str.split('')
   const arr2 = str.toLowerCase().split('')
   const obj = {};
@@ -9,15 +8,12 @@ const firstNonRepeatingLetter = str => {
   for (i = 0; i < arr2.length; i++) {
     obj[arr2[i]] ? obj[arr2[i]]++ : obj[arr2[i]] = 1
   }
-  console.log(obj)
   for (j = 0; j < arr.length; j++) {
     if (obj[arr[j].toLowerCase()] === 1 && count < 1) {
-      console.log(arr[j], 'is the first non-repeating letter')
       return arr[j]
       count++
     }
   }
-  console.log('no repeating letters')
   return ""
 }
 
